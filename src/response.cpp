82eb5ef69634840945578ed13f7878c20dde674b
InJung Chung <mu29gl@gmail.com>
2015-08-17 13:37:46 +0900

    parameter parsing

--- src/response.cpp
+++ src/response.cpp
@@ -13,6 +13,37 @@ std::string Response::GetRoute() {
 bool Response::IsMatch(Request& req) {
     if (req.GetMethod() != method_)
         return false;
+    
+    std::string route = "";
+    int routeSize = 0;
+    
+    std::vector<std::string> params;
+    std::vector<std::string> resRoute = split(route_, '/');
+    
+    for (std::string s : resRoute) {
+        if (s.at(0) == ':') {
+            params.push_back(s.erase(0, 1));
+        } else {
+            routeSize++;
+            route += s + "/";
+        }
+    }
+    
+    std::vector<string> reqRoute = split(req.GetRoute(), '/');
+    
+    if (resRoute.size() != reqRoute.size())
+        return false;
+    
+    for (int i = 0; i < routeSize; i++)
+        if (resRoute[i] != reqRoute[i])
+            return false;
+    
+    for (int i = routeSize; i < reqRoute.size(); i++) {
+        Json::Object& reqParams = req.GetParameter();
+        std::string paramKey = params[i - routeSize];
+        std::string paramValue = reqRoute[i];
+        reqParams[paramKey] = paramValue;
+    }
 
     return true;
 }
