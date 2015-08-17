2ad79751855c2b95612086bc178e462a742fa5de
InJung Chung <mu29gl@gmail.com>
2015-08-17 13:35:42 +0900

    move main file to src folder

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include "fcgi_stdio.h"
-
-int main() {
-	while (FCGI_Accept() >= 0) {
-		printf("test");
-	}
-	return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "fcgi_stdio.h"
+
+int main() {
+    while (FCGI_Accept() >= 0) {
+        printf("test");
+    }
+    return 0;
+}
