8556f77eda1d1e01c044266a58c2bf7fe2f68d96
InJung Chung <mu29gl@gmail.com>
2015-08-17 16:07:12 +0900

    server test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
+#include "../lib/json/lightjson.h"
+#include "../lib/string/split.h"
+
+#include "router.h"
+#include "url_dispatcher.h"
 #include "fcgi_stdio.h"
 
+void LoadFunctions();
+
 int main() {
+    Router* router = Router::GetInstance();
+    UrlDispatcher* urlDispatcher = UrlDispatcher::GetInstance();
+    LoadFunctions();
+
     while (FCGI_Accept() >= 0) {
-        printf("test");
+        Request request = urlDispatcher->GetRequest();
+        router->Handle(request);
+        printf("Content-type:text/html \r\n\r\n hi");
     }
     return 0;
+}
+
+void LoadFunctions() {
+
 }
